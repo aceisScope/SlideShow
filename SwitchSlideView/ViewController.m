e78424da99dabd163774e3e4d0b6a9b4f947d3b7
Bertie Liu <bertieliu52@gmail.com>
2012-07-19 10:50:37 +0800

    put SwitchSlideView in a UITableViewCell

--- SwitchSlideView/ViewController.m
+++ SwitchSlideView/ViewController.m
@@ -9,12 +9,14 @@
 #import "ViewController.h"
 #import "SwitchSlideView.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic,retain) SwitchSlideView *switchSlide;
+@property (nonatomic,retain) UITableView *tableView;
 @end
 
 @implementation ViewController
 @synthesize switchSlide=_switchSlide;
+@synthesize tableView=_tableView;
 
 - (void)viewDidLoad
 {
@@ -23,9 +25,14 @@
     NSArray *images= [NSArray arrayWithObjects:@"01.jpg",@"02.png",@"03.jpg" ,nil];
     
     self.switchSlide = [[SwitchSlideView alloc]initWithFrame:CGRectMake(0, 10, 320, 240)];
-    [self.view addSubview:self.switchSlide];
     [self.switchSlide setImagesWithArray:images];
-
+    //[self.view addSubview:self.switchSlide];
+    
+    self.tableView = [[UITableView alloc]initWithFrame:self.view.frame];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    self.tableView.backgroundColor = [UIColor lightGrayColor];
 }
 
 - (void)viewDidUnload
@@ -37,6 +44,34 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1; 
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";  
+    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if(cell == nil)
+    {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+    [cell addSubview:self.switchSlide];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 260.f;
 }
 
 @end
